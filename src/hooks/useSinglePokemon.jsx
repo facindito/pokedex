6fe57a5c2e051ce1fb5aa0f313229fcb00071876
Facundo <facinditto@gmail.com>
2022-04-01 14:29:53 -0300

    Refactor

--- src/hooks/useSinglePokemon.jsx
+++ src/hooks/useSinglePokemon.jsx
@@ -11,7 +11,7 @@ export function useSinglePokemon ({ keyword }) {
         setPokemonsFilter(pokemon)
       })
     localStorage.setItem('lastKeyword', keywordToUse)
-  }, [keyword, keywordToUse, setPokemonsFilter])
+  }, [keyword, keywordToUse, setPokemonsFilter, pokemonsFilter])
 
   return { pokemonsFilter }
 };
