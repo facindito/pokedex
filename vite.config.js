8b68981440de72f96205031d95b56de945c18417
Facundo <facinditto@gmail.com>
2022-04-01 15:55:32 -0300

    Refactor

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,43 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), disableHistoryFallback]
 })
+function disableHistoryFallback () {
+  const path = require('path')
+  const fs = require('fs')
+
+  const ALLOWLIST = [
+    // internal requests
+    /^\/__vite_ping/,
+    /^\/@vite\/client/,
+    /^\/@id/,
+    /^\/__open-in-editor/,
+
+    // no check needed
+    /^\/$/,
+    /^\/index.html/
+  ]
+  return {
+    name: 'disable-history-fallback',
+    configureServer (server) {
+      server.middlewares.use((req, res, next) => {
+        if (ALLOWLIST.some(pattern => pattern.test(req.url))) {
+          return next()
+        }
+
+        const url = req.url
+          .replace(/^@/, 'src') // replace common path alias
+          .split('?')[0] // ignore query parameters (cache busts)
+
+        if (!fs.existsSync(path.join(__dirname, url))) {
+          console.warn('URL not found:', url)
+          res.statusCode = 404
+          res.end()
+        } else {
+          next()
+        }
+      })
+    }
+  }
+}
